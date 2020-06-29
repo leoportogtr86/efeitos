b56307d008b43a4eab6fe98354c6a2d3c8280154
Leo Porto <leonardoreisporto@gmail.com>
2020-06-29 05:56:22 -0300

    Updates

---
+++ precache-manifest.734ebfea0f2d117a7a00400893a1dd88.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0c9ffc5167cbe0b83532ba4bf7599542",
+    "url": "/efeitos/index.html"
+  },
+  {
+    "revision": "a362fd9729eaee8a80af",
+    "url": "/efeitos/static/css/main.e0a25659.chunk.css"
+  },
+  {
+    "revision": "9540f8dd5f4b18140968",
+    "url": "/efeitos/static/js/2.6ff80ad2.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/efeitos/static/js/2.6ff80ad2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a362fd9729eaee8a80af",
+    "url": "/efeitos/static/js/main.8649f555.chunk.js"
+  },
+  {
+    "revision": "dcac515f1622cbe10320",
+    "url": "/efeitos/static/js/runtime-main.3eb4e010.js"
+  },
+  {
+    "revision": "900034497d43c6b8b5f64aea56d64c0f",
+    "url": "/efeitos/static/media/chorus.90003449.svg"
+  },
+  {
+    "revision": "d111bda04372a025d2f4e9e4b4f27b15",
+    "url": "/efeitos/static/media/delay.d111bda0.svg"
+  },
+  {
+    "revision": "5d0f62f040e9afbe235e7bc0e5c4cccf",
+    "url": "/efeitos/static/media/distorcao.5d0f62f0.svg"
+  },
+  {
+    "revision": "f1a43f8276fb84694e6e796640738310",
+    "url": "/efeitos/static/media/equalizador.f1a43f82.svg"
+  },
+  {
+    "revision": "d62ae827a021578437e068305ae3938a",
+    "url": "/efeitos/static/media/flanger.d62ae827.svg"
+  },
+  {
+    "revision": "eb75bb32e46eec5df4c501d6bffb4587",
+    "url": "/efeitos/static/media/overdrive.eb75bb32.svg"
+  },
+  {
+    "revision": "dd92c3b883356f5d5c072887879d7543",
+    "url": "/efeitos/static/media/phaser.dd92c3b8.svg"
+  },
+  {
+    "revision": "bd0d23ce4a4998a997e032e8677c2355",
+    "url": "/efeitos/static/media/reverb.bd0d23ce.svg"
+  }
+]);
